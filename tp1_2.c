db7d5fd3dc70349761d091cc37261b8ed4d3cd13
Andrea Sofia De Marco <andreasofiademarco@gmail.com>
2023-03-23 13:33:41 -0300

    punto 4b)

--- tp1_2.c
+++ tp1_2.c
@@ -1,11 +1,17 @@
 #include<stdio.h>
 int cuadrado(int numero1);
+void cuadrado2(int numero1, int *puntero_num1);
 int main(){
    int numero1, valor;
+   int *puntero_num1;
+   puntero_num1=&numero1;
    printf("Ingrese un numero entero:\n");
    scanf("%d",&numero1);
    valor=cuadrado(numero1);
-   printf("El cuadrado de ese numero es :%d",valor);
+   printf("El cuadrado de ese numero es :%d\n",valor);
+   cuadrado2(numero1,puntero_num1);
+   printf("El cuadrado de ese numero es :%d\n",numero1);
+
    return 0;
 }
 
@@ -14,4 +20,10 @@ int cuadrado(int numero1){
     valor=(numero1*numero1);
 
     return valor;
+}
+
+void cuadrado2(int numero1 ,int *puntero_num1){
+    *puntero_num1=(numero1*numero1);
+
+
 }
