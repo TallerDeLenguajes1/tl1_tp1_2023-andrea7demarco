7cd389d2be2b5a9fed6aefda4abd76306dd89ec1
Andrea Sofia De Marco <andreasofiademarco@gmail.com>
2023-03-23 17:17:26 -0300

    punto 2 en c

--- tp1_2.c
+++ tp1_2.c
@@ -1,16 +1,24 @@
 #include<stdio.h>
 int cuadrado(int numero1);
 void cuadrado2(int numero1, int *puntero_num1);
+void Invertir(int a,int b);//deberá devolver en el valor de a en la
+//variable b, y en el valor de b el valor de a
 int main(){
-   int numero1, valor;
+   int numero1, valor,a ,b;
    int *puntero_num1;
    puntero_num1=&numero1;
    printf("Ingrese un numero entero:\n");
    scanf("%d",&numero1);
+   printf("Ingrese dos numeros para invertir:\n");
+   scanf("%d",&a);
+   scanf("%d",&b);
+
    valor=cuadrado(numero1);
    printf("El cuadrado de ese numero es :%d\n",valor);
    cuadrado2(numero1,puntero_num1);
    printf("El cuadrado de ese numero es :%d\n",numero1);
+
+   printf("numeros a invertir ingresados: a:%d y b:%d\n",a,b);
 
    return 0;
 }
@@ -28,3 +36,4 @@ void cuadrado2(int numero1 ,int *puntero_num1){
 
 
 }
+
